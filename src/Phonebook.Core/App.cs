5b3d93721d2c51cb5514a2f0c553c1cd13dc68b1
nelaps <neldnelaps@gmail.com>
2018-12-04 14:11:10 +0700

    click button + change text

--- src/Phonebook.Core/App.cs
+++ src/Phonebook.Core/App.cs
@@ -1,12 +1,17 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
 using Phonebook.Core.ViewModels;
 
 namespace Phonebook.Core
-{ 
+{
     public class App : MvxApplication
     {
         public override void Initialize()
         {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
             RegisterAppStart<FirstViewModel>();
         }
     }

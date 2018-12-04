07b8f4bf6ad92e5d88277120253d485e01d04cd7
nelaps <neldnelaps@gmail.com>
2018-12-04 10:10:14 +0700

    Phonebook.iOS

---
+++ src/Phonebook.iOS/Views/FirstView.cs
@@ -0,0 +1,27 @@
+﻿
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using MvvmCross.iOS.Views.Presenters.Attributes;
+using Phonebook.Core.ViewModels;
+using System;
+
+namespace Phonebook.iOS.Views
+{
+    [MvxFromStoryboard]
+    public partial class FirstView : MvxViewController
+    {
+        public FirstView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+            //set.Bind(Label).To(vm => vm.Hello);
+            //set.Bind(TextField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
+}
